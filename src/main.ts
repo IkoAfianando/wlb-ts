5e9cd14e44096450b754fd77a8afd42f0e27b302
IkoAfianando <ikoafianando123@gmail.com>
2022-07-18 08:16:21 +0700

    Update

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
-  await app.listen(8000);
+  await app.listen(process.env.PORT || 8000)
 }
 bootstrap();
